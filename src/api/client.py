9fad0b3ef12ae72e86d5ccb3f069de7800a0b5a4
chandojo <celena.handojo@gmail.com>
2024-09-06 20:35:37 -0700

    Added Client class

---
+++ src/api/client.py
@@ -0,0 +1,17 @@
+import requests
+
+
+class Client:
+    def __init__(self, url, headers):
+        self.url = url
+        self.headers = headers
+
+    def get(self, params=None):
+        try:
+            response = requests.get(
+                    self.url,
+                    headers=self.headers,
+                    params=params)
+            return response
+        except Exception as e:
+            raise e
